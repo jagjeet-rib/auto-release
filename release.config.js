39a29d658a9247f99b1c2bd20a7d162190721d6a
Jagjeet Singh <jagjeet.singh@rib-software.com>
2024-10-10 16:47:15 +0530

    breaking: major update

--- release.config.js
+++ release.config.js
@@ -1,10 +1,20 @@
 module.exports = {
-    branches: ['main'],
-    plugins: [
-      '@semantic-release/commit-analyzer',
-      '@semantic-release/release-notes-generator',
-      // '@semantic-release/npm',
-      '@semantic-release/github'
-    ]
-  };
-  
+  branches: ["main"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    "@semantic-release/git",
+    "@semantic-release/github"
+  ],
+  release: {
+    analyzeCommits: {
+      preset: "conventionalcommits",
+      releaseRules: [
+        {type: "breaking", release: "major"},
+        {type: "feat", release: "minor"},
+        {type: "fix", release: "patch"}
+      ]
+    }
+  }
+};
