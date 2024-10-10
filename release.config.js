e794e0de3d9d66770f615f487be6a994c98e92ca
Jagjeet Singh <jagjeet.singh@rib-software.com>
2024-10-10 19:04:57 +0530

    fix: fix release config for v3

--- release.config.js
+++ release.config.js
@@ -4,7 +4,10 @@ module.exports = {
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
-    "@semantic-release/git",
+    ["@semantic-release/git", {
+      "assets": ["dist/**/*.{js,css}", "docs", "package.json"],
+      "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+    }],
     "@semantic-release/github"
   ],
   release: {
